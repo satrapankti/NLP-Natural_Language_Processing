de916a6e86c558d5590df61613d16d4cdeb3a644
Pankti Satra <satrapankti26@gmail.com>
2023-01-06 20:53:37 +0530

    Update Amazon_Review_Sentiment_Analysis.py

--- Sentiment_Analysis/Amazon_Review_Sentiment_Analysis.py
+++ Sentiment_Analysis/Amazon_Review_Sentiment_Analysis.py
@@ -38,7 +38,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local("amazon.png") 
+add_bg_from_local() 
 
 
 st.title("**Sentiment Analysis of Amazon Reviews**")
